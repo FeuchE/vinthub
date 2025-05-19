b973681081255841b680e483c7b16d4778bffae7
Ellen Feuchtwanger <feuche@gmail.com>
2025-05-19 17:38:26 +0100

    Edited bookings controller actions and views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,7 @@
 class BookingsController < ApplicationController
+  before_action :set_user, only: %i[new create]
+  before_action :set_item, only: %i[new create]
+
   def index
     @bookings = Booking.all
   end
@@ -13,14 +16,25 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
+    @booking.user = @user
+    @booking.item = @item
+
     if @booking.save
-      redirect_to bookings
+      redirect_to booking_path(@booking)
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
