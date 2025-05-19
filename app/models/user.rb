8a8074982abef3282cdf78cf9317ffcc51f71264
Ellen Feuchtwanger <feuche@gmail.com>
2025-05-19 18:44:51 +0100

    Associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :booked_items, through: :bookings, source: :item
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
