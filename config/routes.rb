9505a04d2009d31b914b1cade22c15fbfdb31890
Matt Casey <matthew.casey@hotmail.co.uk>
2025-05-20 13:07:03 +0100

    added faker gem, added seeds, commented out incomplete validations.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "items#index"
 
   resources :items do
     resources :bookings, only: %i[index new create]
