ec511ce113f1f6929bd0c2f6456af6a46e3f5abc
Ellen Feuchtwanger <feuche@gmail.com>
2025-05-20 14:11:49 +0100

    Items - new and create actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,25 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @booking = Booking.new
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+
+    if @item.save
+      redirect_to item_path(@item), notice: 'Item was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price_per_day, :location, :photo)
+  end
 end
