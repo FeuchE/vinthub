8a68012c68a9b03a20c6972115a9dd70bcad27a3
Ellen Feuchtwanger <feuche@gmail.com>
2025-05-23 10:25:25 +0100

    Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
+  # Uncomment when you *really understand* Pundit!
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] == "pages"
+    # ~ /(^(rails_)?admin)|(^pages$)/
+  end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
+  # Pundit: allow-list approach
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
   def index
     if params[:query].present?
       @items = Item.search_by_title_and_description_and_category_and_brand_and_size(params[:query])
@@ -9,22 +13,33 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    authorize @item
     @booking = Booking.new
   end
 
   def new
     @item = Item.new
+    authorize @item
   end
 
   def create
     @item = Item.new(item_params)
     @item.user = current_user
+    authorize @item
 
     if @item.save
       redirect_to item_path(@item), notice: 'Item was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.user = current_user
+    authorize @item
+    @item.destroy
+    redirect_to items_path, notice: 'Item was successfully removed.'
   end
 
   private
